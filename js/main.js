a4f5ebf51d58ec2f0ca133456cdab04a8c28be5b
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-04-03 23:33:19 -0400

    -Added win/loss messages

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 const winLossTexts = [
   {
     name: "win message",
-    "button text": "You've Won!",
+    "button text": "You Win!",
   },
   {
     name: "lose message",
@@ -27,27 +27,28 @@ const playQuitBtns = [
 ];
 
   /*----- state variables -----*/
-let board;
-let turn;
-let winner;
-
+let board;  // Array ref 4 color selections
+let turn;  // Cur player turn --> 0/1
+let winSwtc;
+let cpuNums = [];
+let playerNums = [];
 
   /*----- cached elements  -----*/
 // text messages
 const winLossText = document.querySelector("#winLossText");
 
-
+/*
 // color selections
-const redBtn = document.querySelector('#redBtn').addEventListener('click', );
-const grnBtn = document.querySelector('#grnBtn').addEventListener('click', );
-const bluBtn = document.querySelector('#bluBtn').addEventListener('click', );
-const orgBtn = document.querySelector('#orgBtn').addEventListener('click', );
+const redBtn = document.querySelector('#redBtn').addEventListener('click', slctRed);
+const grnBtn = document.querySelector('#grnBtn').addEventListener('click', slctGrn);
+const bluBtn = document.querySelector('#bluBtn').addEventListener('click', slctBlu);
+const orgBtn = document.querySelector('#orgBtn').addEventListener('click', slctOrg);
 
 // controls
 const playStartBtn = document.querySelector('#startGame').addEventListener('click', playStart);
 const playAgainBtn = document.querySelector('#playAgain').addEventListener('click', playAgain);
 const quirGameBtn = document.querySelector('#quitGame').addEventListener('click', quitGame);
-
+*/
 
   /*----- event listeners -----*/
 
@@ -56,9 +57,16 @@ const quirGameBtn = document.querySelector('#quitGame').addEventListener('click'
 /*----- functions -----*/
 // init();
 
-board = [redBtn, grnBtn, bluBtn, orgBtn];
 
+function winLossFunc(winLoss) {
+  if (winLoss === 'w') {
+    winLossText.innerText = winLossTexts[0]["button text"];
+  } else if (winLoss === 'l') {
+    winLossText.innerText = winLossTexts[1]["button text"];
+  }
+}
 
+winLossFunc(winSwtc);
 
 
 
