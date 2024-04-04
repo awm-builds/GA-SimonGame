09d5d36a4f29cdab04b3eda483bcea2f7bfa8fde
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-04-03 22:01:46 -0400

    -Added win/lose text object

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
   /*----- constants -----*/
 
 // text messages
+const winLossText = document.querySelector("#winLossText");
 
 
 // color selections
-document.querySelector('#startGame').addEventListener('click', playStart);
-document.querySelector('#startGame').addEventListener('click', playStart);
-document.querySelector('#startGame').addEventListener('click', playStart);
-document.querySelector('#startGame').addEventListener('click', playStart);
+const redBtn = document.querySelector('#redBtn').addEventListener('click', playStart);
+const grnBtn = document.querySelector('#grnBtn').addEventListener('click', playStart);
+const bluBtn = document.querySelector('#bluBtn').addEventListener('click', playStart);
+const orgBtn = document.querySelector('#orgBtn').addEventListener('click', playStart);
 
 // controls
 const playStartBtn = document.querySelector('#startGame').addEventListener('click', playStart);
 const playAgainBtn = document.querySelector('#playAgain').addEventListener('click', playAgain);
 const quirGameBtn = document.querySelector('#quitGame').addEventListener('click', quitGame);
-  
+
+// Objects
+const winLossTexts = [
+  {
+    name: "win message",
+    "button text": "You've Won!",
+  },
+  {
+    name: "lose message",
+    "button text": "You Lose.",
+  }
+];
 
   /*----- state variables -----*/
 
